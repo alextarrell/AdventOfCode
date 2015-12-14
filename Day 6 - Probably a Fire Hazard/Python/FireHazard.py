82eec00e48de01ef6c254b594054e7fa59429848
Alex Tarrell <alextarrell@gmail.com>
2015-12-14 08:05:36 -0600

    Added Python Solution to Day 6 Part 2.

--- Day 6 - Probably a Fire Hazard/Python/FireHazard.py
+++ Day 6 - Probably a Fire Hazard/Python/FireHazard.py
@@ -3,10 +3,10 @@
 
 import re
 
-def init_lights(width, height):
+def init_lights(width, height, val=False):
 	lights = []
 	for _ in xrange(height):
-		lights.append([False for _ in xrange(width)])
+		lights.append([val for _ in xrange(width)])
 	return lights
 
 def switch(lights, x1, y1, x2, y2, lightsOn):
@@ -19,8 +19,14 @@ def toggle(lights, x1, y1, x2, y2):
 		for y in xrange(y1, y2):
 			lights[x][y] = not lights[x][y]
 
+def add_brightness(lights, x1, y1, x2, y2, amount):
+	for x in xrange(x1, x2):
+		for y in xrange(y1, y2):
+			val = lights[x][y] + amount
+			lights[x][y] = val if val > 0 else 0
+
 ad = re.compile(r'([ a-z]+) (\d+),(\d+) through (\d+),(\d+)')
-def apply_direction(lights, direction):
+def apply_direction(lights, direction, v2=False):
 	if not direction:
 		return
 
@@ -33,11 +39,20 @@ def apply_direction(lights, direction):
 		y2 = int(m.group(5))
 
 		if action == 'turn on':
-			switch(lights, x1, y1, x2+1, y2+1, True)
+			if v2:
+				add_brightness(lights, x1, y1, x2+1, y2+1, 1)
+			else:
+				switch(lights, x1, y1, x2+1, y2+1, True)
 		elif action == 'turn off':
-			switch(lights, x1, y1, x2+1, y2+1, False)
+			if v2:
+				add_brightness(lights, x1, y1, x2+1, y2+1, -1)
+			else:
+				switch(lights, x1, y1, x2+1, y2+1, False)
 		elif action == 'toggle':
-			toggle(lights, x1, y1, x2+1, y2+1)
+			if v2:
+				add_brightness(lights, x1, y1, x2+1, y2+1, 2)
+			else:
+				toggle(lights, x1, y1, x2+1, y2+1)
 		else:
 			raise ValueError('Unknown Action: ' + action)
 	else:
@@ -51,12 +66,23 @@ def count_lights(lights):
 				total += 1
 	return total
 
+def sum_brightness(lights):
+	total = 0
+	for x in xrange(len(lights)):
+		total += sum(lights[x])
+	return total
+
 def main():
 	lights = init_lights(1000, 1000)
 	directions = get_input()
 	for d in directions:
 		apply_direction(lights, d)
 	print count_lights(lights)
+
+	lights = init_lights(1000, 1000, 0)
+	for d in directions:
+		apply_direction(lights, d, True)
+	print sum_brightness(lights)
 
 def get_input():
 	with open('../day_6_input.txt') as directions:
