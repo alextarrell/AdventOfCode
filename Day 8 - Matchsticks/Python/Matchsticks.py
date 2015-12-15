f1e0796891ac1b751314624ffc3becdb7566b72d
Alex Tarrell <alextarrell@gmail.com>
2015-12-15 08:11:15 -0600

    Day 8 Part 2 solution. Moar regex.

--- Day 8 - Matchsticks/Python/Matchsticks.py
+++ Day 8 - Matchsticks/Python/Matchsticks.py
@@ -10,11 +10,20 @@ lt = re.compile(r'\\x[0-f]{2}|\\[\\"]|.')
 def len_true(line):
 	return len(lt.findall(line[1:-1]))
 
+le = re.compile(r'("|\\|\')')
+def len_escaped(line):
+	return len('"' + le.sub(r'\\\1', line) + '"')
+
 def main():
 	lines = get_input()
 	total_diff = 0
 	for l in lines:
 		total_diff += len_code(l) - len_true(l)
+	print total_diff
+
+	total_diff = 0
+	for l in lines:
+		total_diff += len_escaped(l) - len_code(l)
 	print total_diff
 
 def get_input():
