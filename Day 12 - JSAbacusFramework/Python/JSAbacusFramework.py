d4e14c32890e6c41d787c47775df41912e6bed39
Alex Tarrell <alextarrell@gmail.com>
2015-12-16 23:02:28 -0600

    Added Day 12 Part 2 Python solution. My poor regex was no match...

--- Day 12 - JSAbacusFramework/Python/JSAbacusFramework.py
+++ Day 12 - JSAbacusFramework/Python/JSAbacusFramework.py
@@ -1,17 +1,30 @@
 #! /usr/bin/env python
 # Day 12 - JSAbacusFramework.io
 
-import re
+import json
 
-nf = re.compile(r'(-?\d+)')
+def sum_node(node, ignore_key=None):
+	if isinstance(node, int):
+		return node
+	elif isinstance(node, str):
+		try:
+			return int(node)
+		except ValueError:
+			return 0
+	elif isinstance(node, dict):
+		if ignore_key and ignore_key in node.values():
+			return 0
+		else:
+			return sum([sum_node(n, ignore_key) for n in node.itervalues()])
+	elif isinstance(node, list):
+		return sum([sum_node(n, ignore_key) for n in node])
+	else:
+		return 0
+
 def main():
-	json_doc = get_input()
-
-	total = 0
-	for m in nf.finditer(json_doc):
-		total += int(m.group(1))
-
-	print total
+	json_doc = json.loads(get_input())
+	print sum_node(json_doc)
+	print sum_node(json_doc, 'red')
 
 def get_input():
 	with open('../day_12_input.txt') as json_doc:
