86f17886fa69f73d57eb4b52847f65555b33c700
Alex Tarrell <alextarrell@gmail.com>
2016-12-08 21:47:09 -0600

    Day 2 Part 2

--- 2016/Day 02 - Bathroom Security/Python/Bathroom Security.py
+++ 2016/Day 02 - Bathroom Security/Python/Bathroom Security.py
@@ -4,29 +4,66 @@
 def clamp(val, low, high):
     return max(min(high, val), low)
 
+def get_code(start, directions, keypad):
+	code = []
+	x, y = next(k for k, v in keypad.iteritems() if v == start)
+	for line in directions:
+		for i in line:
+			if i == 'U' and (x, y - 1) in keypad:
+				y -= 1
+			elif i == 'D' and (x, y + 1) in keypad:
+				y += 1
+			elif i == 'L' and (x - 1, y) in keypad:
+				x -= 1
+			elif i == 'R' and (x + 1, y) in keypad:
+				x += 1
+
+		code.append(keypad[(x, y)])
+
+	return ''.join(code)
+
 def main():
 	directions = get_input()
 
-	code = []
-	x, y = 1, 1
-	for line in directions:
-		for i in line:
-			if i == 'U':
-				y -= 1
-			elif i == 'D':
-				y += 1
-			elif i == 'L':
-				x -= 1
-			elif i == 'R':
-				x += 1
+	# 1 2 3
+	# 4 5 6
+	# 7 8 9
+	keypad = {
+		(0, 0): '1',
+		(1, 0): '2',
+		(2, 0): '3',
+		(0, 1): '4',
+		(1, 1): '5',
+		(2, 1): '6',
+		(0, 2): '7',
+		(1, 2): '8',
+		(2, 2): '9'
+	}
 
-			x = clamp(x, 0, 2)
-			y = clamp(y, 0, 2)
+	print "The bathroom code is: {}".format(get_code('5', directions, keypad))
 
-		code.append(str(y * 3 + x + 1))
+	#     1
+	#   2 3 4
+	# 5 6 7 8 9
+	#   A B C
+	#     D
+	keypad = {
+		(2, 0): '1',
+		(1, 1): '2',
+		(2, 1): '3',
+		(3, 1): '4',
+		(0, 2): '5',
+		(1, 2): '6',
+		(2, 2): '7',
+		(3, 2): '8',
+		(4, 2): '9',
+		(1, 3): 'A',
+		(2, 3): 'B',
+		(3, 3): 'C',
+		(2, 4): 'D'
+	}
 
-	print "The bathroom code is: {}".format(''.join(code))
-
+	print "The real bathroom code is: {}".format(get_code('5', directions, keypad))
 
 def get_input():
 	with open('../day_2_input.txt') as stats:
