f26fed51797205037efad1dfef45fad5ecd2acf7
Alex Tarrell <alextarrell@gmail.com>
2016-12-10 19:37:06 -0600

    Day 3 Part 2

--- 2016/Day 03 - Squares With Three Sides/Python/ThreeSidedSquares.py
+++ 2016/Day 03 - Squares With Three Sides/Python/ThreeSidedSquares.py
@@ -1,12 +1,21 @@
 #! /usr/bin/env python
 # Day 3 - Squares With Three Sides
 
+from itertools import chain, islice
+
+def chunk(it, size):
+    return iter(lambda: tuple(islice(it, size)), ())
+
 def main():
 	triangles = get_input()
 
-	valid = [verts for verts in triangles if 2 * max(verts) < sum(verts)]
+	valid_rows = [verts for verts in triangles if 2 * max(verts) < sum(verts)]
 
-	print "Of the {} listed triangles, {} are possible".format(len(triangles), len(valid))
+	print "Of the {} listed triangles, {} are possible horizontally".format(len(triangles), len(valid_rows))
+
+	valid_verticals = [verts for verts in chunk(chain.from_iterable(zip(*triangles)), 3) if 2 * max(verts) < sum(verts)]
+
+	print "When interpreted vertically, {} are valid".format(len(valid_verticals))
 
 def get_input():
 	with open('../day_3_input.txt') as triangles:
