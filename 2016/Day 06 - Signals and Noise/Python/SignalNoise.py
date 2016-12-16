0e81d5c29d667e623772cf7a0aa3107cda5f5e25
Alex Tarrell <alextarrell@gmail.com>
2016-12-15 23:27:26 -0600

    Day 6 Part 1 - A little bit too easy…

---
+++ 2016/Day 06 - Signals and Noise/Python/SignalNoise.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# Day 6 - Signals and Noise
+
+from collections import Counter
+
+def main():
+	signals = get_input()
+
+	corrected = ''.join([Counter(s).most_common(1)[0][0] for s in zip(*signals)])
+	print 'The error-corrected message is {}'.format(corrected)
+
+def get_input():
+	with open('../day_6_input.txt') as data:
+		return [d.strip() for d in data]
+
+
+if __name__ == "__main__":
+	main()
