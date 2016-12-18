583b867a1a12eb48d3a9a783ce33855fd724591c
Alex Tarrell <alextarrell@gmail.com>
2016-12-17 21:30:25 -0600

    Day 9 Part 2

--- 2016/Day 09 - Explosives in Cyberspace/Python/CyberspaceExplosives.py
+++ 2016/Day 09 - Explosives in Cyberspace/Python/CyberspaceExplosives.py
@@ -5,25 +5,33 @@ import re
 
 r_marker = re.compile(r'\((\d+)x(\d+)\)')
 
+def decom(f, recurse=True):
+	file_length = 0
+	start = 0
+	while start < len(f):
+		marker = r_marker.search(f, start)
+		if not marker:
+			file_length += len(f[start:])
+			break
+
+		length = int(marker.group(1))
+		repeat = int(marker.group(2))
+
+		file_length += len(f[start:marker.start()])
+		if recurse:
+			file_length += decom(f[marker.end():marker.end() + length]) * repeat
+		else:
+			file_length += len(f[marker.end():marker.end() + length]) * repeat
+
+		start = marker.end() + length
+
+	return file_length
+
 def main():
 	files = get_input()
 
 	for idx, f in enumerate(files):
-		out = ""
-		start = 0
-		while start < len(f):
-			marker = r_marker.search(f, start)
-			if not marker:
-				out += f[start:]
-				break
-
-			length = int(marker.group(1))
-			repeat = int(marker.group(2))
-
-			out += f[start:marker.start()] + f[marker.end():marker.end() + length] * repeat
-			start = marker.end() + length
-
-		print 'The decompressed length of file {} is {}'.format(idx + 1, len(out))
+		print 'The decompressed length of file {} is {}, or {} with recursion'.format(idx + 1, decom(f, False), decom(f))
 
 def get_input():
 	with open('../day_9_input.txt') as data:
