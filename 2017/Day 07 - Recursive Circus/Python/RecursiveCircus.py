9735348f7ba23dd13385dd8b230bd27f42740c30
Alex Tarrell <alextarrell@gmail.com>
2017-12-13 00:37:14 -0600

    Day 7 P2 Python

--- 2017/Day 07 - Recursive Circus/Python/RecursiveCircus.py
+++ 2017/Day 07 - Recursive Circus/Python/RecursiveCircus.py
@@ -6,6 +6,7 @@ sys.path.append("../../..")
 
 from python_util import prints, get_input
 import re
+from collections import Counter
 
 re_struct = re.compile(r"(\w+) \((\d+)\)(?: -> (.*))?")
 
@@ -13,7 +14,7 @@ class Tower():
 	def __init__(self, structure):
 		match = re_struct.match(structure)
 		self.name = match.group(1)
-		self.weight = int(match.group(2))
+		self._weight = int(match.group(2))
 		self.subs = filter(None, (match.group(3) or "").split(', '))
 		self.parent = None
 
@@ -22,11 +23,19 @@ class Tower():
 		for tower in self.subs:
 			tower.parent = self
 
+	def weight(self):
+		if not hasattr(self, '__weight'):
+			setattr(self, '__weight', self._weight + sum(tower.weight() for tower in self.subs))
+		return getattr(self, '__weight')
+
+	def balanced(self):
+		return all(self.subs[0].weight() == s.weight() for s in self.subs)
+
 	def __str__(self):
 		if self.subs:
-			return "{} ({}) -> [{}]".format(self.name, self.weight, ', '.join(str(s) for s in self.subs))
+			return "{} ({}) -> [{}]".format(self.name, self._weight, ', '.join(str(s) for s in self.subs))
 		else:
-			return "{} ({})".format(self.name, self.weight)
+			return "{} ({})".format(self.name, self._weight)
 	__repr__ = __str__
 
 def main():
@@ -51,12 +60,16 @@ def main():
 	prints(sample, find_bottom, 'tknk')
 	prints(get_input(), find_bottom)
 
+	print "*** Part 2 ***"
+	prints(sample, find_imbalance, 60)
+	prints(get_input(), find_imbalance)
+
 def construct(structures):
 	towers = [Tower(s) for s in structures]
 	lookup = {s.name: s for s in towers}
 	for s in towers:
 		s.link(lookup)
-
+		
 	tower = towers[0]
 	while tower.parent:
 		tower = tower.parent
@@ -64,6 +77,16 @@ def construct(structures):
 
 def find_bottom(structures):
 	return construct(structures).name
+
+def find_imbalance(structures):
+	tower = construct(structures)
+	while not tower.balanced():
+		if all(s.balanced() for s in tower.subs):
+			break
+		tower = next(s for s in tower.subs if not s.balanced())
+	target, imbalance = tuple(c[0] for c in Counter(s.weight() for s in tower.subs).most_common())
+	imbalanced_tower = next(s for s in tower.subs if s.weight() == imbalance)
+	return imbalanced_tower._weight + target - imbalance
 
 if __name__ == '__main__':
 	main()
