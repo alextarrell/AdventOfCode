d707bb0a62366c38cfea9fd6176edd34742228d2
Alex Tarrell <alextarrell@gmail.com>
2017-12-12 23:11:37 -0600

    Day 6 P2 Python Yay for codd reuse

--- 2017/Day 06 - Memory Reallocation/Python/MemoryReallocation.py
+++ 2017/Day 06 - Memory Reallocation/Python/MemoryReallocation.py
@@ -9,9 +9,15 @@ from python_util import prints, get_input
 def main():
 	print "*** Part 1 ***"
 
-	prints('0 2 7 0', reallocate, 5)
+	prints('0 2 7 0', reallocate, ('2 4 1 2', 5))
 	for banks in get_input():
 		prints(banks.replace('\t', ' '), reallocate)
+
+	print "*** Part 2 ***"
+
+	prints('0 2 7 0', loop, 4)
+	for banks in get_input():
+		prints(banks.replace('\t', ' '), loop)
 
 def reallocate(banks):
 	banks = [int(b) for b in banks.split(' ')]
@@ -27,7 +33,11 @@ def reallocate(banks):
 			banks[idx] += 1
 			bucket -= 1
 
-	return len(uniqs)
+	return ' '.join(str(b) for b in banks), len(uniqs)
+
+def loop(banks):
+	banks, _ = reallocate(banks)
+	return reallocate(banks)[1]
 
 if __name__ == '__main__':
 	main()
