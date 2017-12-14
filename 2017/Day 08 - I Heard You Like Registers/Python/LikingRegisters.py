308dc3771d34b5a4866fffc08ef318ec987c7153
Alex Tarrell <alextarrell@gmail.com>
2017-12-13 23:22:19 -0600

    Day 8 P2 Python Reorganized things a bit for clarity

--- 2017/Day 08 - I Heard You Like Registers/Python/LikingRegisters.py
+++ 2017/Day 08 - I Heard You Like Registers/Python/LikingRegisters.py
@@ -11,8 +11,6 @@ from collections import defaultdict
 re_struct = re.compile(r"(\w+) (inc|dec) ((?:-)?\d+) if (\w+) (<|<=|==|!=|>=|>) ((?:-)?\d+)")
 
 def main():
-	print "*** Part 1 ***"
-
 	sample = [
 		"b inc 5 if a > 1",
 		"a inc 1 if b < 5",
@@ -20,12 +18,29 @@ def main():
 		"c inc -20 if c == 10"
 	]
 
+	print "*** Part 1 ***"
 	prints(sample, get_max_reg, 1)
 	prints(get_input(), get_max_reg)
+	
+	print "*** Part 2 ***"
+	prints(sample, track_max_reg, 10)
+	prints(get_input(), track_max_reg)
 
 def get_max_reg(instructions):
-	registers = process(instructions)
+	registers = defaultdict(int)
+	for line in instructions:
+		compute(line, registers)
 	return max(registers.values())
+
+def track_max_reg(instructions):
+	registers = defaultdict(int)
+	mx = None
+	for line in instructions:
+		compute(line, registers)
+		local_max = max(registers.values())
+		if local_max > mx:
+			mx = local_max
+	return mx
 
 cond_lookup = {
 	'<': (lambda t, v: t < v),
@@ -35,13 +50,6 @@ cond_lookup = {
 	'>=': (lambda t, v: t >= v),
 	'>': (lambda t, v: t > v)
 }
-
-def process(instructions):
-	registers = defaultdict(int)
-	for line in instructions:
-		compute(line, registers)
-
-	return dict(registers)
 
 def compute(line, registers):
 	match = re_struct.match(line)
