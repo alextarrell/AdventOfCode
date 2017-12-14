f3bb0fb2b1bb9a789924208666fc4f3feccdb075
Alex Tarrell <alextarrell@gmail.com>
2017-12-13 23:17:08 -0600

    Day 8 P1 Python

---
+++ 2017/Day 08 - I Heard You Like Registers/Python/LikingRegisters.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+# Day 8 - I Heard You Like Registers
+
+import sys
+sys.path.append("../../..")
+
+from python_util import prints, get_input
+import re
+from collections import defaultdict
+
+re_struct = re.compile(r"(\w+) (inc|dec) ((?:-)?\d+) if (\w+) (<|<=|==|!=|>=|>) ((?:-)?\d+)")
+
+def main():
+	print "*** Part 1 ***"
+
+	sample = [
+		"b inc 5 if a > 1",
+		"a inc 1 if b < 5",
+		"c dec -10 if a >= 1",
+		"c inc -20 if c == 10"
+	]
+
+	prints(sample, get_max_reg, 1)
+	prints(get_input(), get_max_reg)
+
+def get_max_reg(instructions):
+	registers = process(instructions)
+	return max(registers.values())
+
+cond_lookup = {
+	'<': (lambda t, v: t < v),
+	'<=': (lambda t, v: t <= v),
+	'==': (lambda t, v: t == v),
+	'!=': (lambda t, v: t != v),
+	'>=': (lambda t, v: t >= v),
+	'>': (lambda t, v: t > v)
+}
+
+def process(instructions):
+	registers = defaultdict(int)
+	for line in instructions:
+		compute(line, registers)
+
+	return dict(registers)
+
+def compute(line, registers):
+	match = re_struct.match(line)
+	if not match:
+		raise Exception("Failed to parse " + line)
+
+	target = match.group(1)
+	op = match.group(2)
+	amount = int(match.group(3))
+	cond_val = registers[match.group(4)]
+	cond = match.group(5)
+	cond_amount = int(match.group(6))
+
+	if cond_lookup[cond](cond_val, cond_amount):
+		if op == 'inc':
+			registers[target] += amount
+		elif op == 'dec':
+			registers[target] -= amount
+		else:
+			raise Exception("Unknown operator " + op)
+
+	return registers
+
+if __name__ == '__main__':
+	main()
