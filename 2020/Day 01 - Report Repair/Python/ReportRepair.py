2abb72d6a905784192935bc4f7cd086f0963412e
Alex Tarrell <alextarrell@gmail.com>
2020-12-06 00:40:31 -0600

    2020 Day 1 P2 Python

--- 2020/Day 01 - Report Repair/Python/ReportRepair.py
+++ 2020/Day 01 - Report Repair/Python/ReportRepair.py
@@ -1,7 +1,12 @@
 # Day 1 - Report Repair
 import sys
 
-def find_sum(data, target):
+def find_duo_sum(data, target):
+	if not data or len(data) < 2:
+		return None
+	elif len(data) == 2:
+		return (data[0], data[1]) if sum(data == target) else None
+
 	start = 0
 	end = len(data) - 1
 	while start < end:
@@ -11,20 +16,39 @@ def find_sum(data, target):
 		elif total < target:
 			start += 1
 		else:
-			break
-	else:
-		return None
+			return (data[start], data[end])
 
-	return (data[start], data[end])
+	return None
+
+def find_tri_sum(data, target):
+	if not data or len(data) < 3:
+		return None
+	elif len(data) == 3:
+		return (data[0], data[1], data[2]) if sum(data == target) else None
+
+	start = 0
+	while start < len(data):
+		result = find_duo_sum(data[start:], target - data[start])
+		if result:
+			return data[start], result[0], result[1]
+		else:
+			start += 1
+	return None
 
 def main():
 	data = sorted(int(line) for line in sys.stdin.readlines())
 
-	result = find_sum(data, 2020)
+	result = find_duo_sum(data, 2020)
 	if not result:
 		print('No Solution')
 	else:
 		print(result[0] * result[1])
+
+	result = find_tri_sum(data, 2020)
+	if not result:
+		print('No Solution')
+	else:
+		print(result[0] * result[1] * result[2])
 
 if __name__ == '__main__':
 	main()
