c2243e261ede8d7c04b69b2da7811c353b68807e
Alex Tarrell <alextarrell@gmail.com>
2020-12-09 23:19:48 -0600

    2020 Day 6 P2 Python

--- 2020/Day 06 - Custom Customs/Python/CustomCustoms.py
+++ 2020/Day 06 - Custom Customs/Python/CustomCustoms.py
@@ -16,14 +16,18 @@ def parse_input(data):
 
 	return groups
 
-def uniqs(responses):
+def union(responses):
 	return set().union(*responses)
+
+def intersection(responses):
+	return set.intersection(*responses)
 
 def main():
 	data = (line.strip() for line in sys.stdin.readlines())
 
 	groups = parse_input(data)
-	print(sum(len(uniqs(r)) for r in groups))
+	print(sum(len(union(r)) for r in groups))
+	print(sum(len(intersection(r)) for r in groups))
 
 if __name__ == '__main__':
 	main()
