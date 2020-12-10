de8b25c66414f6f956cc4ea22293707334d9d9cd
Alex Tarrell <alextarrell@gmail.com>
2020-12-09 22:48:30 -0600

    2020 Day 5 P2 Python

--- 2020/Day 05 - Binary Boarding/Python/BinaryBoarding.py
+++ 2020/Day 05 - Binary Boarding/Python/BinaryBoarding.py
@@ -14,11 +14,27 @@ def decode(coords, rows, cols):
 	column = int(coords[st:].replace('L', '0').replace('R', '1'), 2)
 	return Seat(row, column)
 
+def find_seat(seats):
+	if len(seats) < 3:
+		return None
+
+	seats = sorted(s.id for s in seats)
+	lookup = set(seats)
+	for _id in range(seats[1], seats[-1]):
+		if _id not in lookup:
+			return Seat(int(_id / 8), _id % 8)
+	return None
+
 def main():
 	data = (line.strip() for line in sys.stdin.readlines())
 
 	seats = [decode(row, 128, 8) for row in data]
 	print(max(s.id for s in seats))
+
+	if (seat := find_seat(seats)):
+		print(seat.id)
+	else:
+		print('Not Found')
 
 if __name__ == '__main__':
 	main()
