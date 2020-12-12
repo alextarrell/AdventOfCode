e2443c5f11cd66d4565c834eca032490a7c24b7f
Alex Tarrell <alextarrell@gmail.com>
2020-12-11 22:11:15 -0600

    2020 Day 7 P2 Python

--- 2020/Day 07 - Handy Haversacks/Python/HandyHaversacks.py
+++ 2020/Day 07 - Handy Haversacks/Python/HandyHaversacks.py
@@ -1,6 +1,6 @@
 # Day 7 - Handy Haversacks
 import sys, re
-from collections import namedtuple
+from collections import defaultdict, namedtuple
 
 Rule = namedtuple('Rule', ('color', 'contents'))
 def parse_rule(rule):
@@ -15,11 +15,21 @@ def can_contain(rules, color):
 			allowed.update(can_contain(rules, rule.color))
 	return allowed
 
+def nested_bags(rules, color):
+	rule = next(r for r in rules if r.color == color)
+	contained = defaultdict(int, **rule.contents)
+	for c, n in rule.contents.items():
+		for ci, ni in nested_bags(rules, c).items():
+			contained[ci] += ni * n
+
+	return contained
+
 def main():
 	data = (line.strip() for line in sys.stdin.readlines())
 
 	rules = [parse_rule(r) for r in data]
 	print(len(can_contain(rules, 'shiny gold')))
+	print(sum(nested_bags(rules, 'shiny gold').values()))
 
 if __name__ == '__main__':
 	main()
