6c4370b483f1f80555271ad11eff5dc44bacf502
Alex Tarrell <alextarrell@gmail.com>
2020-12-13 20:57:30 -0600

    2020 Day 9 P2 Python

--- 2020/Day 09 - Encoding Error/Python/EncodingError.py
+++ 2020/Day 09 - Encoding Error/Python/EncodingError.py
@@ -2,10 +2,21 @@
 import sys
 from itertools import combinations
 
-def find_weakness(data, length):
+def find_invalid(data, length):
 	for idx, val in enumerate(data[length:]):
 		if all(sum(pair) != val for pair in combinations(data[idx:idx + length], 2)):
 			return val
+
+def find_weakness(data, invalid):
+	streak = []
+	for start in range(len(data)):
+		for end in range(start + 2, len(data)):
+			result = sum(data[start:end])
+			if result == invalid:
+				streak = data[start:end]
+			elif result > invalid:
+				break
+	return min(streak) + max(streak)
 
 def main():
 	data = [int(line.strip()) for line in sys.stdin.readlines()]
@@ -16,7 +27,9 @@ def main():
 		# data set is short we just assumes it's test data Assumes it's test data
 		preamble_size = 5
 
-	print(find_weakness(data, preamble_size))
+	invalid = find_invalid(data, preamble_size)
+	print(invalid)
+	print(find_weakness(data, invalid))
 
 if __name__ == '__main__':
 	main()
